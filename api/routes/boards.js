8d38ee1a503e1b145a082e61d59ce9a18239c47a
Maksym Zinchenko <maklaut98@gmail.com>
2021-06-17 22:48:35 -0500

    changin delete 2

--- api/routes/boards.js
+++ api/routes/boards.js
@@ -9,7 +9,7 @@ router.get('/getboards/:userName', BoardsController.boards_get_all);
 
 router.post('/:userid', checkAuth, BoardsController.boards_create_board);
 
-router.post('/', BoardsController.deleate_all_boards);
+router.post('/deleteAllBoards', BoardsController.deleate_all_boards);
 router.post('/deleteBoard', BoardsController.deleate_board);
 
 module.exports = router;
