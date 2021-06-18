df165d5b7e0dc871d8340753cdc642247e8a75ab
Maksym Zinchenko <maklaut98@gmail.com>
2021-06-17 21:21:02 -0500

    header changed back

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers", 
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-      "Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if(req.method==='OPTIONS'){
       req.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH, PUT');
